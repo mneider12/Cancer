1c5c296cecf65a4758b1d1b44f97718a99cabd27
Mark Nyder <mneider.virginia@gmail.com>
2015-12-28 16:26:51 -0600

    Add tests for mother / father in Person

--- Cancer/test/pedigreetest/PersonTest.java
+++ Cancer/test/pedigreetest/PersonTest.java
@@ -29,6 +29,12 @@ public class PersonTest {
 		Person fullTest = new Person(new Trait("Carrot Cake"), Gender.female);
 		assertEquals("Carrot Cake", fullTest.getTrait().getName());
 		assertEquals(Gender.female, fullTest.getGender());
+		Person mother = new Person(new Trait("mother"), Gender.female);
+		Person father = new Person(new Trait("father"), Gender.male);
+		Person parentTest = new Person(new Trait("born"), Gender.male, mother, father);
+		assertEquals("born", parentTest.getTrait().getName());
+		assertEquals(mother, parentTest.getMother());
+		assertEquals(father, parentTest.getFather());
 	}
 	
 	/*
@@ -52,6 +58,19 @@ public class PersonTest {
 		Trait trait = new Trait("Strawberry");
 		person.setTrait(trait);
 		assertEquals("Strawberry", person.getTrait().getName());
+	}
+	
+	/*
+	 * Test getter and setter for mother and father
+	 */
+	@Test
+	public void setParentTest() {
+		Person mother = new Person();
+		Person father = new Person();
+		person.setMother(mother);
+		person.setFather(father);
+		assertEquals(mother, person.getMother());
+		assertEquals(father, person.getFather());
 	}
 }
 
